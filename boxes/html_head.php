3e596b57ae84b576645cb52234eb42470df6fc04
Paolo Moretti <littlebrown@gmail.com>
2014-01-18 17:53:08 +0000

    general Fix

--- boxes/html_head.php
+++ boxes/html_head.php
@@ -1,31 +1,47 @@
-<?php global $env; ?>
-
+<?php global $context; ?>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 	<title>Rigorix - Gioco online a premi</title>
 	<meta name="description" content="Gioca gratis e vinci bellissimi premi ai rigori. Rigorix e' uno dei piu' divertenti giochi online gratuiti in flash in cui si vincono veri premi. Gioca online!">
 	<meta name="keywords" content="giochi on line gratuiti a premi">
-//	<style type="text/css">
-//		@import "/app/assets/dist/dist.common.css";
-//		@import "/css/common.css";
-//		@import "/css/ui/jquery-ui-1.8.1.custom.css";
-//		@import "/css/colorPicker.css";
-//		<?php if ( isset ($context) && $context == 'dialog' ) { ?>
-//		.dialog-default-container { padding: 0 !important; }
-//		<?php } ?>
-//		/*@import "/css/developing.css";*/
-//	</style>
-//	<script>
-//		var root = '';
-//		var dev = <?php echo ( $_SESSION['rigorix']['settings']['DEVELOPER'] == true ) ? "true" : "false"; ?>;
-//		<?php if ( isset ($context) && $context == 'dialog' ) { ?>
-//		var DialogWindow = this;
-//		<?php } ?>
-//	</script>
+	<style type="text/css">
+		@import "/app/assets/dist/dist.common.css";
+		@import "/css/common.css";
+		@import "/css/ui/jquery-ui-1.8.1.custom.css";
+		@import "/css/colorPicker.css";
+		<?php if ( isset ($context) && $context == 'dialog' ) { ?>
+		.dialog-default-container { padding: 0 !important; }
+		<?php } ?>
+		/*@import "/css/developing.css";*/
+	</style>
+	<script>
+		var root = '';
+		var dev = <?php echo ( $_SESSION['rigorix']['settings']['DEVELOPER'] == true ) ? "true" : "false"; ?>;
+		<?php if ( isset ($context) && $context == 'dialog' ) { ?>
+		var DialogWindow = this;
+		<?php } ?>
+
+		var User = <? echo ( is_object($user->obj) ? $utility->print_json ( $user->obj ) : 'false' ); ?>;
+    var RigorixEnv = <?php echo json_encode($env); ?>;
+	</script>
+
+	<!-- script src="/app/assets/vendor/jquery-1.10.2.min.js" type="text/javascript"></script>
+
+	<script src="/app/assets/vendor/angular.min.js" type="text/javascript"></script>
+	<script src="/app/assets/vendor/angular-route.min.js" type="text/javascript"></script>
+  <script src="/app/assets/vendor/angular-resource.min.js" type="text/javascript"></script>
+  <script src="/app/config.js" type="text/javascript"></script>
+  <script src="/app/app.js" type="text/javascript"></script>
+  <script src="/app/services/services.js" type="text/javascript"></script>
+  <script src="/app/services/auth.js" type="text/javascript"></script>
+  <script src="/app/controllers/Main.js" type="text/javascript"></script>
+  <script src="/app/controllers/Sidebar.js" type="text/javascript"></script>
+  <script src="/app/controllers/Header.js" type="text/javascript"></script>
+  <script src="/app/controllers/Home.js" type="text/javascript"></script -->
 
   <script src="/app/assets/dist/dist.js" type="text/javascript"></script>
 
-  <script src="/js/query-ui-1.8.1.custom.min.js" type="text/javascript"></script>
+  <script src="/js/jquery-ui-1.8.1.custom.min.js" type="text/javascript"></script>
 	<script src="/js/jquery-ui-1.8.1.custom.min.js" type="text/javascript"></script>
 	<script src="/js/j_core.js" type="text/javascript"></script>
 	<script src="/js/j_activity.js" type="text/javascript"></script>
@@ -36,18 +52,15 @@
 	<script src="/js/activity.fb.js" type="text/javascript"></script>
 	<script src="/js/colorPicker/colorPicker.js" type="text/javascript"></script>
 	<script>
-//	<? if( isset ($msg) && strlen($msg) > 0) { ?>
-//		action_queue.push(function() {
-//			win.html('<? echo $msg; ?>');
-//			win.dialog("open");
-//		});
-//	<? } ?>
-//	<? if( isset ($_REQUEST["show_tab"]) && $_REQUEST["show_tab"] != '' ) { ?>
-//		end_action_queue.push(function() { activity.ui.show_tab ('<?=$_REQUEST["show_tab"]?>'); });
-//	<? }
-
-	var User = <?php echo ( is_object($user->obj) ? json_encode($user->obj) : 'false' ); ?>;
-	var RigorixEnv = <?php echo json_encode($env); ?>
+	<? if( isset ($msg) && strlen($msg) > 0) { ?>
+		action_queue.push(function() {
+			win.html('<? echo $msg; ?>');
+			win.dialog("open");
+		});
+	<? } ?>
+	<? if( isset ($_REQUEST["show_tab"]) && $_REQUEST["show_tab"] != '' ) { ?>
+		end_action_queue.push(function() { activity.ui.show_tab ('<?=$_REQUEST["show_tab"]?>'); });
+	<? } ?>
 	</script>
 	<!-- OAS SETUP begin -->
 	<SCRIPT type="text/javascript">
